76f6de823ad1c56f2a5e88dc8f3a73400dace4a5
Bright mabuza <mabuza6@gmail.com>
2020-04-13 13:47:25 +0200

    added user login functions

--- src/app/containers/user/login/login.component.ts
+++ src/app/containers/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../../../_interfaces/user.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  user: User = {
+    email: '',
+    username: '',
+    fullName: '',
+    password: ''
+  };
+  loginFailedMsg: string;
+  error: string;
+  constructor(
+    private route: Router,
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loginFailedMsg = '';
+  }
+  async onSubmit(data) {
+    this.loginFailedMsg = '';
+    this.error = '';
+    this.user.email = data.value.Email;
   }
 
+  Register() {
+    this.route.navigate(['/register']);
+  }
 }
