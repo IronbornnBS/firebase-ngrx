87fe0d79c9ffed61cd5315fa9657d93b32d16c65
Bright mabuza <mabuza6@gmail.com>
2020-04-13 13:48:11 +0200

    registration functions

--- src/app/containers/user/register/register.component.ts
+++ src/app/containers/user/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/_interfaces/user.model';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = {
+    email: '',
+    username: '',
+    fullName: '',
+    password: ''
+  };
+  formValid: boolean;
 
-  ngOnInit(): void {
+  constructor(
+    private route: Router,
+  ) { }
+
+  ngOnInit() {
   }
 
+  async onSubmit(data) {
+    // this.user.userName = data.value.Username;
+    // this.user.fullName = `${data.value.FirstName} ${data.value.LastName}`;
+    // this.user.email = data.value.Email;
+    // if (data.value.Password === data.value.ConfirmPassword) {
+    //   this.user.password = data.value.Password;
+    //   await this.userService.createUser(this.user)
+    //     .then(res => {
+    //       if (res !== null) {
+    //         this.toastr.successToastr('Registration Completed, Please login', 'Success!');
+    //         this.route.navigate(['/login']);
+    //       }
+    //     })
+    //     .catch(err => {
+    //       this.toastr.errorToastr(err.message, 'Error!');
+    //     });
+    // } else {
+    //   this.toastr.errorToastr('Password must match Confirm Password', 'Error!');
+    // }
+  }
+
+  onFormChanged(formData) {
+    if (
+      formData.value.FirstName === undefined ||
+      formData.value.LastName === undefined ||
+      formData.value.Username === undefined ||
+      formData.value.Password === undefined ||
+      formData.value.ConfirmPassword === undefined ||
+      formData.value.Email === undefined
+    ) {
+      return;
+    }
+
+    if (formData.value.Username.length !== 0) {
+      if (formData.value.Email.length >= 5) {
+        if (formData.value.firstname.length !== 0) {
+          if (formData.value.Lastname.length !== 0) {
+            if (formData.value.Password.length === formData.value.ConfirmPassword.length) {
+              this.formValid = true;
+              return;
+            } else {
+              this.formValid = false;
+            }
+          } else {
+            this.formValid = false;
+          }
+        } else {
+          this.formValid = false;
+        }
+      } else {
+        this.formValid = false;
+      }
+    } else {
+      this.formValid = false;
+    }
+  }
+
+  navigate() {
+    this.route.navigate(['/login']);
+  }
 }
