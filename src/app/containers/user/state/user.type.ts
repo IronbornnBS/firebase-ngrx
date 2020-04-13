b6656510bc8a358c8b14677068ddae10eda3c265
Bright mabuza <mabuza6@gmail.com>
2020-04-13 22:35:20 +0200

    new types

--- src/app/containers/user/state/user.type.ts
+++ src/app/containers/user/state/user.type.ts
@@ -1,4 +1,10 @@
 export enum UserActionTypes {
   SetCurrentUser = '[User] Set Current User',
   ClearCurrentUser = '[User] Clear Current User',
+  Login = '[User] Login',
+  LoginSuccess = '[User] Login Success',
+  LoginFail = '[User] Login Fail',
+  Register = '[User] Register',
+  RegisterSuccess = '[User] Register Success',
+  RegisterFail = '[User] Register Fail',
 }
