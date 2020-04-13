4e51763b6600c98e161d70984bee2ab145a51865
Bright mabuza <mabuza6@gmail.com>
2020-04-13 22:38:19 +0200

    removed unnecessary function

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -5,28 +5,26 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { User } from '../../_interfaces/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   private path = 'users';
   collection: AngularFirestoreCollection<User>;
 
+  constructor(
+    private firestore: AngularFirestore,
+    private fireauth: AngularFireAuth
+  ) {
+    this.collection = this.firestore.collection(this.path, (ref) =>
+      ref.orderBy('firstName', 'asc')
+    );
+  }
 
-  constructor(private firestore: AngularFirestore, private fireauth: AngularFireAuth) {
-    this.collection = this.firestore.collection(this.path, ref => ref.orderBy('firstName', 'asc'));
-   }
+  login = (email: string, password: string) =>
+    this.fireauth.signInWithEmailAndPassword(email, password)
 
-   registration = (data: User) => this.register(data.email, data.password)
-   .then(response  => {
-     data.uid = response.user.uid;
-     data.email = response.user.email.toLowerCase();
-     return this.create(data).then(results => results);
-   })
+  register = (email: string, password: string) =>
+    this.fireauth.createUserWithEmailAndPassword(email, password)
 
-   login = (email: string, password: string) => this.fireauth.signInWithEmailAndPassword(email, password);
-
-   create = (data: User) => this.collection.add(data);
-
-   register = (email: string, password: string) => this.fireauth.createUserWithEmailAndPassword(email, password);
-
+  create = (data: User) => this.collection.add(data);
 }
