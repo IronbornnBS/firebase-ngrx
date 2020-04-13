341793c742fa8aeeeadb3e8d32f173dd077edb4f
Bright mabuza <mabuza6@gmail.com>
2020-04-13 22:38:00 +0200

    declared user effect

--- src/app/containers/user/user.module.ts
+++ src/app/containers/user/user.module.ts
@@ -8,7 +8,9 @@ import { RegisterComponent } from './register/register.component';
 
 /* NgRx */
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { reducer } from './state/user.reducer';
+import { UserEffect } from './state/user.effect';
 
 const userRoutes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -19,7 +21,10 @@ const userRoutes: Routes = [
   imports: [
     SharedModule,
     RouterModule.forChild(userRoutes),
-    StoreModule.forFeature('users', reducer)
+    StoreModule.forFeature('users', reducer),
+    EffectsModule.forFeature(
+      [ UserEffect ]
+    ),
   ],
   declarations: [
     LoginComponent,
