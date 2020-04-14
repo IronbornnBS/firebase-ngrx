7011ffa0fff5f1d90c66fad4a82e84a714925524
Bright mabuza <mabuza6@gmail.com>
2020-04-15 01:10:08 +0200

    annuity effects class

---
+++ src/app/containers/annuity/state/annuity.effects.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { AnnuityService } from 'src/app/shared/services/annuity.service';
+import { AnnuitiesActionTypes } from './annuity.type';
+import * as annuitiesActions from './annuity.actions';
+import { mergeMap, map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnnuityEffect {
+  constructor(
+    private actions$: Actions,
+    private annuityService: AnnuityService
+  ) {}
+
+  @Effect()
+  loadAnnuities$ = this.actions$.pipe(
+    ofType(AnnuitiesActionTypes.Load),
+    mergeMap((action: annuitiesActions.Load) =>
+      this.annuityService.get().pipe(
+        map((data) => data.map((annuities) => new annuitiesActions.LoadSuccess([annuities.payload.doc.data()]) ),
+        catchError(err => of(new annuitiesActions.LoadFail(err)))
+      )
+    )
+  )
+);
+
+}
