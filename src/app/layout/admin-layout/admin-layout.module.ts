2cfb8df5c0dc623aeeb2f76ecc56c6acd8221160
Bright mabuza <mabuza6@gmail.com>
2020-04-15 01:39:05 +0200

    added annuity state to the store

--- src/app/layout/admin-layout/admin-layout.module.ts
+++ src/app/layout/admin-layout/admin-layout.module.ts
@@ -5,12 +5,18 @@ import { RouterModule } from '@angular/router';
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { AnnuityListComponent } from 'src/app/containers/annuity/annuity-list/annuity-list.component';
 import { MaintainanceListComponent } from 'src/app/containers/maintainance/maintainance-list/maintainance-list.component';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from 'src/app/containers/annuity/state/annuity.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { AnnuityEffect } from 'src/app/containers/annuity/state/annuity.effects';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(AdminLayoutRoutes)
+    RouterModule.forChild(AdminLayoutRoutes),
+    StoreModule.forFeature('annuities', reducer),
+    EffectsModule.forFeature([AnnuityEffect])
   ],
   exports: [],
   declarations: [
