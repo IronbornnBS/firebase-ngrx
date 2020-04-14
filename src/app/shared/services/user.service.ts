f7b01adb9dbfe77bd904507d42f02c7fa293d8dc
Bright mabuza <mabuza6@gmail.com>
2020-04-14 23:41:13 +0200

    removed uneccessary functions

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFirestoreCollection } from '@angular/fire/firestore/public_api';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { User } from '../../_interfaces/user.model';
@@ -13,13 +12,8 @@ export class UserService {
   collection: AngularFirestoreCollection<User>;
 
   constructor(
-    private firestore: AngularFirestore,
     private fireauth: AngularFireAuth
-  ) {
-    this.collection = this.firestore.collection(this.path, (ref) =>
-      ref.orderBy('firstName', 'asc')
-    );
-  }
+  ) { }
 
   login = (email: string, password: string) =>
     from(this.fireauth.signInWithEmailAndPassword(email, password))
@@ -27,5 +21,5 @@ export class UserService {
   register = (email: string, password: string) =>
     from(this.fireauth.createUserWithEmailAndPassword(email, password))
 
-  create = (data: User) => this.collection.add(data);
+  logout = () => from(this.fireauth.signOut());
 }
