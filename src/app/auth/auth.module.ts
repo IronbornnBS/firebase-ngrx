0c81f7c685c521f7218e5a11182eb870cf1b2081
Bright mabuza <mabuza6@gmail.com>
2020-04-20 10:37:03 +0200

    initialized auth state and effect

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -10,6 +10,10 @@ import { MatRippleModule } from '@angular/material/core';
 
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { AuthEffects } from './store/auth.effects';
+import * as fromAuth from './store/auth.reducer';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
@@ -19,6 +23,8 @@ import { RegisterComponent } from './components/register/register.component';
     BrowserAnimationsModule,
     FormsModule,
     RouterModule,
+    StoreModule.forFeature('auth', fromAuth.authReducer),
+    EffectsModule.forFeature([AuthEffects]),
     MatFormFieldModule,
     MatInputModule,
     MatRippleModule
