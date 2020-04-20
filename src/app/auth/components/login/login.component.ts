10b37000afeb35e607c6f109b15ca703250697d3
Bright mabuza <mabuza6@gmail.com>
2020-04-20 11:47:38 +0200

    auth functionality fully functional

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { AppState } from 'src/app/reducers';
+import * as actions from './../../store/auth.actions';
+import { getError } from '../../store/auth.selectors';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -11,13 +16,29 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   error$: Observable<string | null>;
 
-  constructor() {}
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', Validators.required),
     });
+
+    this.error$ = this.store
+    .pipe(
+      select(getError),
+      map( (error: any) => {
+        if (error) {
+          if (error.code === 'auth/weak-password') {
+            return error.message;
+          } else if (error.code === 'auth/email-already-in-use') {
+            return 'User with this email address already exist';
+          }
+        } else {
+          return null;
+        }
+      })
+    );
   }
 
   public get email() {
@@ -30,7 +51,7 @@ export class LoginComponent implements OnInit {
 
   onLogin = () => {
     if (this.loginForm.valid) {
-      // dispatch store auth action
+      this.store.dispatch(new actions.LoginRequested(this.loginForm.value));
     }
   }
 }
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { AppState } from '../../../reducers';
 import * as actions from './../../store/auth.actions';
+import { getError } from '../../store/auth.selectors';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -22,6 +24,22 @@ export class RegisterComponent implements OnInit {
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
+
+    this.error$ = this.store
+    .pipe(
+      select(getError),
+      map( (error: any) => {
+        if (error) {
+          if (error.code === 'auth/weak-password') {
+            return error.message;
+          } else if (error.code === 'auth/email-already-in-use') {
+            return 'User with this email address already exist';
+          }
+        } else {
+          return null;
+        }
+      })
+    );
   }
 
   onRegister = () => {
