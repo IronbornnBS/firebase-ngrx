4bdf1cd30be4266ec892c38b94b5c24ae0281f89
Bright mabuza <mabuza6@gmail.com>
2020-04-23 13:31:29 +0200

    added header to module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,15 +5,22 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, WelcomeComponent, DashboardComponent],
+  declarations: [HomeComponent, WelcomeComponent, DashboardComponent, HeaderComponent],
   imports: [
     CommonModule,
     RouterModule,
     MatButtonModule
+  ],
+  exports: [
+    WelcomeComponent,
+    HomeComponent,
+    DashboardComponent,
+    HeaderComponent
   ]
 })
 export class CoreModule { }
